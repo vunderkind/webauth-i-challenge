bbb3ae1433f66e3326003d861165c46013d61073
mogwai <feelirabs@gmail.com>
2019-11-13 02:27:54 +0100

    had to reinstall bcrypt for some reason

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ server.get('/', (req, res) => {
     res.status(200).send('<img src="https://media.giphy.com/media/d3Kq5w84bzlBLVDO/giphy.gif" alt="it\'s alive"/>')
 });
 
-server.get('/api/users', protected, (req, res) => {
+server.get('/api/users', cookieprotection, (req, res) => {
     helper.getAllData()
         .then(data => {
         res.send(data)
