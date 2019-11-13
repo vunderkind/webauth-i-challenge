71d11bea7ed2da91f897007f0cb1ee6ad89ea1af
mogwai <feelirabs@gmail.com>
2019-11-13 01:17:57 +0100

    added new changes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const helper = require('./api/hashHelpers');
+const bcrypt = require('bcryptjs');
+const restricted = require('./api/restricted-middleware');
 
 const server = express();
 
@@ -6,8 +9,51 @@ server.use(express.json());
 
 const port = 5000;
 
+//Some bcrypt voodoo inside
+
+
+
+
+
+//an api called users. When we query it, it returns all the users in the db we created
+
+
 server.get('/', (req, res) => {
     res.status(200).send('<img src="https://media.giphy.com/media/d3Kq5w84bzlBLVDO/giphy.gif" alt="it\'s alive"/>')
 });
 
-server.listen(port, ()=> console.log(`Listening on ${port}!`));
+server.get('/api/users', restricted, (req, res) => {
+    helper.getAllData()
+        .then(data => {
+        res.send(data)
+    });
+});
+
+//register to server
+server.post('/api/register', (req, res) => {
+const credentials = req.body;
+const hash = bcrypt.hashSync(credentials.password, 14);
+credentials.password = hash;
+    helper.add(credentials)
+    .then(data => {
+            return res.status(401).json({message:  `New details: ${data.username}`})
+        });
+
+});
+
+//sign in to server
+
+server.post('/api/login', (req, res) => {
+    const {username, password} = req.body;
+    helper.findByUsername(username)
+    .then(user => {
+    if(!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({error: `Incorrect Creds`})
+    } else {
+        return res.status(200).json({message: `Welcome, ${user.username}!`})
+    }
+    })
+})
+
+
+server.listen(port, () => console.log(`Listening on ${port}!`));
